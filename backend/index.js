1391ead64473609ea49661b5586972f269ecb0fd
mcountryman <countryman.marco@gmail.com>
2022-03-22 18:32:37 -0400

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('MongoDB connected...'))
+    .catch((error) => console.log(error))
 
 app.listen(4000, () => {
     console.log("Server running...")
