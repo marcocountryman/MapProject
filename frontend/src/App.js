098ecf5bee7b1b9c9cb7379c5e4ac83bdee42cf3
mcountryman <countryman.marco@gmail.com>
2022-03-27 12:34:13 -0400

    added marker to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import Map from 'react-map-gl';
+import Map, {Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { FaMapPin } from 'react-icons/fa';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Map
+      <Map
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: -73.985428,
+        latitude: 40.748817,
+        zoom: 12
       }}
-      style={{width: 600, height: 400}}
+      style={{width: "100vw", height: "100vh"}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-    />
+      >
+      <Marker longitude={-73.985428} latitude={40.748817} anchor="bottom" >
+        <FaMapPin className= "map-pin"/>
+      </Marker>  
+    </Map>
     </div>
   );
 }
