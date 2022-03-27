df47c2730289996720cdb9a32d1aa3a30cced5cb
mcountryman <countryman.marco@gmail.com>
2022-03-27 13:44:43 -0400

    setting up pins util

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import Map, {Marker} from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import Map, {Marker, Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { FaMapPin } from 'react-icons/fa';
+import { FaMapPin, FaStar } from 'react-icons/fa';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [ showPopup, setShowPopup ] = useState(true);
+  const [ pins, setPins ] = useState([]);
+
+  useEffect( () => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPins();
+  }, [])
+
+
+
+
   return (
     <div className="App">
       <Map
@@ -19,7 +38,28 @@ function App() {
       >
       <Marker longitude={-73.985428} latitude={40.748817} anchor="bottom" >
         <FaMapPin className= "map-pin"/>
-      </Marker>  
+      </Marker>
+       {/* {showPopup && (
+      <Popup longitude={-73.985428} latitude={40.748817}
+        anchor="left"
+        className = "popup"
+      >
+        <div className = "card">
+          <span>Place</span>
+          <span>Empire State Building</span>
+          <span>Review</span>
+          <span>My Summary</span>
+          <div className='stars'>
+            <span>Rating: </span>
+            <FaStar className = "star"/>
+            <FaStar className = "star"/>
+            <FaStar className = "star"/>
+            <FaStar className = "star"/>
+            <FaStar className = "star"/>
+          </div>
+          <span>Information</span>
+        </div>
+      </Popup>)}   */}
     </Map>
     </div>
   );
